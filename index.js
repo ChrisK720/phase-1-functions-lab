1393afa744882ba44fd71d4ba874f45f4732a561
Christopher Kirathe <ckirathe2005@gmail.com>
2023-12-18 12:09:50 +0300

    1st attepmt

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(location, headquater){
+    let result  
+    if(location >= 42){
+        result = location - headquater
+    }else if (location < 42){
+        result = headquater - location
+    }
+    return result
+    
+}
+
+function distanceFromHqInFeet(location, headquater, ){
+function distanceFromHqInBlocks(location, headquater){
+    let result1  
+    if(location >= 42){
+        result1 = location - headquater
+    }else if (location < 42){
+        result1 = headquater - location
+    }
+    return result1
+ }
+
+ let result2 = result1 * 264
+ return result2
+    
+    
+}
+
+function calculatesFarePrice(start, destination){
+function distanceTravelledInFeet(start, destination) {
+   if (start >= destination) {
+    result1 = (start - destination) * 264
+    }
+   else if (start < destination){
+    result1 = (destination - start) * 264
+
+    }
+}
+  if (result1 < 400){
+    result2 = free
+   } else if (result1 >= 400 && result1 <=2000){
+    result2 = (result1 -400) * 2
+   } else if (result1 > 2000 && result1 < 2500){
+    result2 = "flat fare charge"
+   } else {
+    result = 'cannot travel that far'
+   }
+
+}
